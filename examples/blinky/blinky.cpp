7a00d8f44745f73fd424c31825ebc4135679a726
Frank Leon Rose <frankleonrose@gmail.com>
2019-10-27 14:28:55 -0400

    Added basic blinky app for Adafruit FeatherM0 env

---
+++ examples/blinky/blinky.cpp
@@ -0,0 +1,46 @@
+#include <sam.h>
+#include <stdint.h>
+
+#define LED_PORT PORT_PA17
+
+void toggleLED() {
+  static bool led = false;
+  led = !led;
+
+  if (led) {
+    REG_PORT_OUT0 &= ~LED_PORT;
+  }
+  else {
+    REG_PORT_OUT0 |= LED_PORT;
+  }
+}
+
+void init() {
+  SystemInit();
+
+  // Direct register manipulation, because CMSIS does not provide a GPIO abstraction.
+  REG_PORT_DIR0 |= LED_PORT;
+}
+
+const uint32_t CYCLES_PER_SEC = 1000000; // Initial speed of SAMD21 post reset
+const uint32_t CYCLES_PER_MS = CYCLES_PER_SEC / 1000;
+const uint32_t CYCLES_PER_LOOP = 6;
+const uint32_t LOOPS_PER_MS = CYCLES_PER_MS / CYCLES_PER_LOOP;
+void spinDelay(uint16_t ms) {
+  for (uint16_t d = 0; d<ms; ++d) {
+    for (uint32_t c = 0; c<LOOPS_PER_MS; ++c) {
+      static volatile uint32_t ticks = 0; // Volatile so that loop elided by compiler.
+      ticks = c;
+    }
+  }
+}
+
+int main() {
+  init();
+
+  for( ;; ) {
+    toggleLED();
+
+    spinDelay(500);
+  }
+}
